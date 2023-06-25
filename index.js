dabb55b4a09259356a07620bdaf1933720cfcd64
skybreakbb <willistaylora@gmail.com>
2023-06-24 20:41:44 -0500

    added small error handling for user input, edited questions

--- index.js
+++ index.js
@@ -4,65 +4,24 @@ const prompt = inquirer.createPromptModule();
 const questions = require("./lib/questions");
 const { Shape, Square, Circle, Triangle } = require("./lib/shapes");
 const { genSVGSqr, genSVGCir, genSVGTri } = require("./lib/genSVG");
-// const genSVGSqr = require("./lib/genSVG");
-// const genSVGCir = require("./lib/genSVG");
-// const genSVGTri = require("./lib/genSVG");
 
-// prompt(questions).then((answers) => {
-//     const test = new Circle(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//     test.render();
-//     }
-// );
-// prompt(questions).then((answers) => {
-//     const key = answers.shape;
-//     switch (key) {
-//         case 'Square':
-//             const square = new Square(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//             const genSq = genSVGSqr(square);
-//             fs.writeFileSync('TESTING.svg', genSq);
-//             break;
-//         case 'Circle':
-//             const circle = new Circle(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//             const genCr = genSVGCir(circle);
-//             fs.writeFileSync('TESTING.svg', genCr);
-//             break;
-//         case 'Triangle':
-//             const triangle = new Triangle(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//             const genTr = genSVGTri(triangle);
-//             fs.writeFileSync('TESTING.svg', genTr);
-//             break;
-//         }
-//     }
-// );
-
-// prompt(questions).then((answers) => {
-//     const key = answers.shape;
-//     let genLogo;
-//     let shapeGen;
-//     if (key === 'Square') {
-//         genLogo = new Square(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//         shapeGen = genSVGSqr(genLogo);
-//         console.log("you chose a square")
-//         // fs.writeFileSync('TESTING.svg', genSq);
-//         console.log(shapeGen);
-//         return;
-//     } else if (key === 'Circle') {
-//         genLogo = new Circle(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-//         shapeGen = genSVGCir(answers.text, answers.textColor, answers.shapeColor);
-//         console.log("you chose a circle")
-//         // fs.writeFileSync('TESTING.svg', genCi);
-//         console.log(shapeGen);
-//         return;
-//     } else {
-//         console.log("PICK A SHAPE");
-//     }
-
-// });
 
 prompt(questions).then((answers) => {
+    const hasNumber = /\d/;
+    if (answers.text.length > 3) {
+        answers.text = answers.text.slice(0,3);
+        console.log(`You entered too much text, I shortened it to '${answers.text}' for you.`);
+    }
+    if (hasNumber.test(answers.textColor)) {
+        answers.textColor = `#${answers.textColor}`;
+        console.log(`You seem to have entered a hex color code but didn't add a #! Don't worry, I fixed it for you.`)
+    }
+    if (hasNumber.test(answers.shapeColor)) {
+        answers.textColor = `#${answers.shapeColor}`;
+        console.log(`You seem to have entered a hex color code but didn't add a #! Don't worry, I fixed it for you.`)
+    }
     let key = answers.shapeName;
     let shape;
-    console.log(`you have chosen ${key}.`);
     if (key === 'Square') {
         shape = new Square(answers.text, answers.textColor, answers.shapeName, answers.shapeColor);
         const test = genSVGSqr(shape.text, shape.textColor, shape.shapeColor);
